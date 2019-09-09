1d82c1e82ffdc1e01826472303b1077d5d93ad37
Laura Berge <lauraeberge@gmail.com>
2019-09-09 21:20:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,7 +29,7 @@ end
 def kesha_maker(array)
   new_array = []
   array.each do |string|
-    puts string.split[0]
+    puts string.split[""]
     #letters[2] = "$"
     #new_array << letters.join("")
   end
