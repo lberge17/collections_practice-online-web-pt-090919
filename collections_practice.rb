846d143a608e771729cebc0210d2eee5da9d40f2
Laura Berge <lauraeberge@gmail.com>
2019-09-09 21:22:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,7 +29,8 @@ end
 def kesha_maker(array)
   new_array = []
   array.each do |string|
-    string.split("")[2] = "$"
+    letters = string.split("")
+    letters[2] = "$"
     new_array << letters.join("")
   end
   new_array
